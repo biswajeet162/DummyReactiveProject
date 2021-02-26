3a3a01b72dc68731b04a35585bd117e30cc79571
Biswajeet Mandal <biswajeet163@gmail.com>
2021-02-26 11:51:44 +0530

    Book Reactive Project init

---
+++ src/main/java/com/reactive/book/BookReactiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReactiveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReactiveProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive/book/BookReactiveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReactiveProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
